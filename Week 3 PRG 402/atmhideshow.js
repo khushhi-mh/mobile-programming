e82cbaa907a88a4d1492e4b759667b9e3e022f47
WebPage-K <khushhimaharjan@gmail.com>
2026-05-19 07:37:47 +0545

    jQuery Hide/Show

--- Week 3 PRG 402/atmhideshow.js
+++ Week 3 PRG 402/atmhideshow.js
@@ -4,9 +4,7 @@ let pin = "7539";
 let visible = false;
 
 function checkPin() {
-
     let enteredPin = prompt("Enter PIN:");
-
     if (enteredPin === pin) {
         return true;
     }
@@ -17,44 +15,28 @@ function checkPin() {
 }
 
 function eye() {
-
     if (visible === false) {
-
-        document.getElementById("balanceText").innerText =
-            "Balance: Rs. " + balance;
-
-        document.getElementById("eyeIcon").className =
-            "fa-solid fa-eye";
-
+        document.getElementById("balanceText").innerText = "Balance: Rs. " + balance;
+        document.getElementById("eyeIcon").className = "fa-solid fa-eye";
         visible = true;
     }
     else {
-
-        document.getElementById("balanceText").innerText =
-            "Balance: Rs. ******";
-
-        document.getElementById("eyeIcon").className =
-            "fa-solid fa-eye-slash";
-
+        document.getElementById("balanceText").innerText = "Balance: Rs. ******";
+        document.getElementById("eyeIcon").className = "fa-solid fa-eye-slash";
         visible = false;
     }
 }
 
 function updateBalance() {
-
     if (visible === true) {
-
-        document.getElementById("balanceText").innerText =
-            "Balance: Rs. " + balance;
+        document.getElementById("balanceText").innerText = "Balance: Rs. " + balance;
     }
 }
 
 function showInput(action) {
 
     if (!checkPin()) return;
-
-    document.getElementById("amountSection").style.display =
-        "block";
+        document.getElementById("amountSection").style.display = "block";
 
     if (action === "deposit") {
         deposit();
@@ -66,25 +48,17 @@ function showInput(action) {
 
 function deposit() {
 
-    let amount =
-        Number(document.getElementById("amount").value);
+    let amount = Number(document.getElementById("amount").value);
 
     if (amount <= 0) {
-
-        document.getElementById("message").innerText =
-            "Invalid amount!";
+        document.getElementById("message").innerText = "Invalid amount!";
     }
     else if (amount % 100 !== 0) {
-
-        document.getElementById("message").innerText =
-            "Deposit amount should be multiple of 100!";
+        document.getElementById("message").innerText = "Deposit amount should be multiple of 100!";
     }
     else {
-
         balance += amount;
-
-        document.getElementById("message").innerText =
-            "Deposit successful!";
+        document.getElementById("message").innerText = "Deposit successful!";
     }
 
     updateBalance();
@@ -92,30 +66,20 @@ function deposit() {
 
 function withdraw() {
 
-    let amount =
-        Number(document.getElementById("amount").value);
+    let amount = Number(document.getElementById("amount").value);
 
     if (amount <= 0) {
-
-        document.getElementById("message").innerText =
-            "Invalid amount!";
+        document.getElementById("message").innerText = "Invalid amount!";
     }
     else if (amount % 100 !== 0) {
-
-        document.getElementById("message").innerText =
-            "Withdraw amount should be multiple of 100!";
+        document.getElementById("message").innerText = "Withdraw amount should be multiple of 100!";
     }
     else if (amount > balance) {
-
-        document.getElementById("message").innerText =
-            "Insufficient balance!";
+        document.getElementById("message").innerText = "Insufficient balance!";
     }
     else {
-
         balance -= amount;
-
-        document.getElementById("message").innerText =
-            "Withdrawal successful!";
+        document.getElementById("message").innerText = "Withdrawal successful!";
     }
 
     updateBalance();
